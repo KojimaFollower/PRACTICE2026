3d126c1e340a566f9db5c9651be4e3571cb04c3f
unknown <ilyasgamer1234@mail.ru>
2026-06-28 20:25:12 +0600

    task1

---
+++ practice2026/practice2026/task01/StringExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace task01
+{
+    public static class StringExtensions
+    {
+        public static bool IsPalindrome(this string input)
+        {
+            string lowerword = input.ToLowerInvariant();
+            string newword = "";
+            foreach (char c in lowerword)
+            {
+                if (!char.IsPunctuation(c) && !char.IsWhiteSpace(c))
+                {
+                    newword += c;
+                }
+            }
+            if (newword == new string(newword.Reverse().ToArray()) && newword.Length != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
