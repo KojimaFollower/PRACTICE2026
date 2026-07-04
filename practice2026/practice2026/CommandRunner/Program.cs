abcdeab96b64d0ea9a8ad5266b5a6ca126daa266
unknown <ilyasgamer1234@mail.ru>
2026-07-05 01:37:49 +0600

    task08

---
+++ practice2026/practice2026/CommandRunner/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using CommandLib;
+namespace CommandRunner
+{
+    public class CommandRunner
+    {
+        static void Main()
+        {
+            string DLLPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "FileSystemCommands.dll");
+            Assembly assembly = Assembly.LoadFrom(DLLPath);
+            var types = assembly.GetTypes().Where(type => typeof(ICommand).IsAssignableFrom(type) && type.IsClass).ToList();
+            var commandtypes = assembly.GetTypes().Where(type => typeof(ICommand).IsAssignableFrom(type) && type.IsClass).ToList();
+
+            string DirectoryTest= Environment.CurrentDirectory;
+            string MaskTest = "*";
+
+            foreach (var type in commandtypes)
+            {
+                ICommand command = null;
+                if (type.Name == "DirectorySizeCommand")
+                {
+                    command = (ICommand)Activator.CreateInstance(type, DirectoryTest);
+                }
+                else if (type.Name == "FindFilesCommand")
+                {
+                    command = (ICommand)Activator.CreateInstance(type, DirectoryTest, MaskTest);
+                }
+                command.Execute();
+            }
+        }
+    }
+}
