1afac6aa9a2a6b46756c26b3c489007b5a05f254
unknown <ilyasgamer1234@mail.ru>
2026-07-07 18:08:57 +0600

    task10

---
+++ practice2026/practice2026/CommandLib/CommandLib.cs
@@ -0,0 +1,26 @@
+﻿namespace CommandLib
+{
+    public interface ICommand
+    {
+        void Execute();
+    }
+
+
+    [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+    public class PluginLoadAttribute : Attribute
+    {
+        public Type[] dependencies { get; }
+
+        public PluginLoadAttribute(params Type[] dependencies)
+        {
+            if (dependencies != null)
+            {
+                this.dependencies = dependencies;
+            }
+            else
+            {
+                this.dependencies = Type.EmptyTypes;
+            }
+        }
+    }
+}
