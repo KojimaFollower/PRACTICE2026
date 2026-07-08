3156653c2d0ff5d91c7000121d7a0b6b901cf0be
unknown <ilyasgamer1234@mail.ru>
2026-07-08 23:09:01 +0600

    task11

---
+++ practice2026/practice2026/task11/task11.cs
@@ -0,0 +1,70 @@
+﻿using System.Reflection;
+using task11;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using System.Runtime.Loader;
+
+namespace task11
+{
+    public interface ICalculator
+    {
+        public int Add(int a, int b);
+        public int Minus(int a, int b);
+        public int Mul(int a, int b);
+        public int Div(int a, int b);
+    }
+    public static class CalculatorCreator
+    {
+        public static ICalculator CreateCalculator()
+        {
+            string code = @"public class Calculator
+    {
+        public int Add(int a, int b) => a + b;
+        public int Minus(int a, int b) => a - b;
+        public int Mul(int a, int b) => a * b;
+        public int Div(int a, int b) => a / b;
+    }";
+            string modified = code.Replace("public class Calculator", "public class Calculator : task11.ICalculator");
+            var syntax = CSharpSyntaxTree.ParseText(modified);
+            string assname = Path.GetRandomFileName();
+            var references = new MetadataReference[]
+            {
+            MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
+            MetadataReference.CreateFromFile(typeof(ICalculator).Assembly.Location),
+            MetadataReference.CreateFromFile(Assembly.Load(new AssemblyName("System.Runtime")).Location)
+            };
+
+            var compilation = CSharpCompilation.Create(
+                assname,
+                syntaxTrees: new[] { syntax },
+                references: references,
+                options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+            using var ms = new MemoryStream();
+            var result = compilation.Emit(ms);
+
+            if (!result.Success)
+            {
+                var failures = string.Join("\n", result.Diagnostics.Where(d => d.Severity == DiagnosticSeverity.Error).Select(d => d.GetMessage()));
+                throw new InvalidOperationException("Ошибка компиляции!");
+            }
+
+            ms.Seek(0, SeekOrigin.Begin);
+            var assembly = AssemblyLoadContext.Default.LoadFromStream(ms);
+
+            var type = assembly.GetType("Calculator");
+
+            if (type == null)
+                throw new TypeLoadException("Не удалось найти класс Calculator");
+
+            return (ICalculator)Activator.CreateInstance(type);
+
+        }
+
+        
+
+    }
+
+
+}
+
---
+++ practice2026/practice2026/task11tests/task11tests.cs
@@ -0,0 +1,40 @@
+﻿using task11;
+
+
+namespace task11tests
+{
+    public class task11tests
+    {
+        [Fact]
+        public void CreateCalculator_ShouldPerformAllArithmeticOperationsCorrectly()
+        {
+            ICalculator calculator = CalculatorCreator.CreateCalculator();
+            Assert.Equal(8, calculator.Add(5, 3));
+            Assert.Equal(2, calculator.Minus(5, 3));
+            Assert.Equal(15, calculator.Mul(5, 3));
+            Assert.Equal(2, calculator.Div(6, 3));
+        }
+        [Fact]
+        public void CreateCalculator_DivisionByZero_ShouldThrowDivideByZeroException()
+        {
+            ICalculator calculator = CalculatorCreator.CreateCalculator();
+            Assert.Throws<DivideByZeroException>(() => calculator.Div(10, 0));
+        }
+        [Fact]
+        public void Div_IntMinValueByMinusOne_ShouldThrowOverflowException()
+        {
+            ICalculator calculator = CalculatorCreator.CreateCalculator();
+            Assert.Throws<OverflowException>(() => calculator.Div(int.MinValue, -1));
+        }
+
+        [Fact]
+        public void CreateCalculator_Instance_ShouldNotBeNull()
+        {
+            ICalculator calculator = CalculatorCreator.CreateCalculator();
+            Assert.NotNull(calculator);
+            Assert.IsAssignableFrom<ICalculator>(calculator);
+        }
+
+
+    }
+}
