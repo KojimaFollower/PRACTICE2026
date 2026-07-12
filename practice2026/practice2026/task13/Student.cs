e1434940216831f05c24f2b7b413090267b34207
unknown <ilyasgamer1234@mail.ru>
2026-07-12 16:34:56 +0600

    task13

---
+++ practice2026/practice2026/task13/Student.cs
@@ -0,0 +1,58 @@
+﻿namespace task13
+{
+    public class Subject
+    {
+        public string Name { get; set; }
+        public int Grade { get; set; }
+    }
+
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public List<Subject> Grades { get; set; }
+        public void Validate()
+        {
+            if (string.IsNullOrWhiteSpace(FirstName))
+                throw new ArgumentException("Имя отсутствует!");
+
+            if (string.IsNullOrWhiteSpace(LastName))
+                throw new ArgumentException("Фамилия отсутствует!");
+            foreach (char ch in FirstName)
+            {
+                if (char.IsPunctuation(ch))
+                {
+                    throw new ArgumentException("Имя содержит недопустимые символы");
+                }
+            }
+            foreach (char ch in LastName)
+            {
+                if (char.IsPunctuation(ch))
+                {
+                    throw new ArgumentException("Фамилия содержит недопустимые символы");
+                }
+            }
+
+            if (BirthDate.Year < 1900)
+            {
+                throw new ArgumentException("Некорректная дата рождения (ранее 1900 года)", nameof(BirthDate));
+            }
+            if (BirthDate.Year > 2011)
+            {
+                throw new ArgumentException("Некорректная дата рождения (позже 2011)", nameof(BirthDate));
+            }
+            if (Grades != null)
+            {
+                foreach (var subject in Grades)
+                {
+                    if (string.IsNullOrWhiteSpace(subject.Name))
+                        throw new ArgumentException("Некорректное название предмета!");
+
+                    if (subject.Grade > 100 || subject.Grade < 0)
+                        throw new ArgumentException("Некорректная оценка!");
+                }
+            }
+        }
+    }
+}
---
+++ practice2026/practice2026/task13/StudentSerializer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace task13
+{
+    public class StudentSerializer
+    {
+        private static readonly JsonSerializerOptions Options = new()
+        {
+            Converters = { new DateTimeConvert() },
+            WriteIndented = true,
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+        };
+
+        public static string Serialize(Student student)
+        {
+            return JsonSerializer.Serialize(student, Options);
+        }
+
+        public static Student Deserialize(string json)
+        {
+            var result = JsonSerializer.Deserialize<Student>(json, Options);
+
+            result?.Validate();
+
+            return result;
+        }
+
+        public static void Upload(string path, Student student)
+        {
+            string json = Serialize(student);
+            File.WriteAllText(path, json);
+        }
+
+        public static Student Download(string path)
+        {
+            if (!File.Exists(path))
+                throw new FileNotFoundException($"Файл не найден");
+
+            string json = File.ReadAllText(path);
+            return Deserialize(json);
+        }
+    }
+}
