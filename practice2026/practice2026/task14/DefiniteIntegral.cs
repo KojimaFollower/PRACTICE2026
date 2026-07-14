d725b18d49f381a77044fff921df17669b755834
unknown <ilyasgamer1234@mail.ru>
2026-07-15 03:06:17 +0600

    task14

--- practice2026/practice2026/task14/DefiniteIntegral.cs
+++ practice2026/practice2026/task14/DefiniteIntegral.cs
@@ -1,55 +1,67 @@
-﻿namespace task14
+﻿using System;
+using System.Threading;
+
+public class DefiniteIntegral
 {
-    public class DefiniteIntegral
+    public static double SolveSingleThread(double a, double b, Func<double, double> function, double step)
     {
-        public static double Solve(double a, double b, Func<double, double> function, double step, int threadsnumber)
+        int totalsteps = (int)Math.Ceiling((b - a) / step);
+        double exactstep = (b - a) / totalsteps;
+        double totalarea = 0.0;
+
+        for (int s = 0; s < totalsteps; s++)
         {
-            double totalarea = 0.0;
-            int totalsteps = (int)Math.Ceiling((b - a) / step);
-            int stepsperthread = totalsteps / threadsnumber;
-            Thread[] threads = new Thread[threadsnumber];
-            double exactstep = (b - a) / totalsteps;
-            for (int i = 0; i < threadsnumber; i++)
-            {
-                int start = i * stepsperthread;
-                int end;
-                if (i == threadsnumber - 1)
-                {
-                    end = totalsteps;
-                }
-                else
-                {
-                    end = (i + 1) * stepsperthread;
-                }
-                threads[i] = new Thread(() =>
-                {
-                    double localarea = 0.0;
-                    for (int s = start; s < end; s++)
-                    {
-                        double x1 = a + s * exactstep;
-                        double x2 = x1 + exactstep;
+            double x1 = a + s * exactstep;
+            double x2 = x1 + exactstep;
 
-                        double y1 = function(x1);
-                        double y2 = function(x2);
-                        localarea += (y1 + y2) / 2.0 * exactstep;
-                    }
-                    double initialvalue;
-                    double value;
-                    do
-                    {
-                        initialvalue = totalarea;
-                        value = initialvalue + localarea;
-                    }
-                    while (Interlocked.CompareExchange(ref totalarea, value, initialvalue) != initialvalue);
+            double y1 = function(x1);
+            double y2 = function(x2);
 
-                });
-                threads[i].Start();
-            }
-            foreach (var thread in threads)
-            {
-                thread.Join();
-            }
-            return totalarea;
+            totalarea += (y1 + y2) / 2.0 * exactstep;
         }
+
+        return totalarea;
+    }
+
+    public static double Solve(double a, double b, Func<double, double> function, double step, int threadsnumber)
+    {
+        double totalarea = 0.0;
+        int totalsteps = (int)Math.Ceiling((b - a) / step);
+        int stepsperthread = totalsteps / threadsnumber;
+        Thread[] threads = new Thread[threadsnumber];
+        double exactstep = (b - a) / totalsteps;
+        for (int i = 0; i < threadsnumber; i++)
+        {
+            int start = i * stepsperthread;
+            int end = (i == threadsnumber - 1) ? totalsteps : (i + 1) * stepsperthread;
+            
+            threads[i] = new Thread(() =>
+            {
+                double localarea = 0.0;
+                for (int s = start; s < end; s++)
+                {
+                    double x1 = a + s * exactstep;
+                    double x2 = x1 + exactstep;
+
+                    double y1 = function(x1);
+                    double y2 = function(x2);
+                    localarea += (y1 + y2) / 2.0 * exactstep;
+                }
+                double initialvalue;
+                double value;
+                do
+                {
+                    initialvalue = totalarea;
+                    value = initialvalue + localarea;
+                }
+                while (Interlocked.CompareExchange(ref totalarea, value, initialvalue) != initialvalue);
+            });
+            threads[i].Start();
+        }
+        foreach (var thread in threads)
+        {
+            thread.Join();
+        }
+        return totalarea;
     }
 }
